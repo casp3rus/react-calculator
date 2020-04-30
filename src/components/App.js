8e88ea5e1a573a5d4d61f574a4daf59b93a6b002
casp3rus <adrian.popan@gmail.com>
2020-04-30 16:13:47 -0500

    add and implement mathjs

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import { Button } from './Button'
 import { Display } from './Display'
 import './App.css';
 
+import { evaluate } from 'mathjs'
+
 function App() {
 
-  const [displayText, setDisplayText] = useState('0')
+  const [displayValue, setDisplayValue] = useState('0')
+  const [prevNum, setPrevNum] = useState(null)
+  const [operator, setOperator] = useState(null)
 
   const keys = [
     "AC", "±", "%", "÷",
@@ -15,12 +19,14 @@ function App() {
     '0', '•', '='
   ]
 
-  // Add keyboard event listener 
+  // Add keyboard event listener      
 
   const handleKeyDown  = ({key}) => {
 
     console.log('Pressed key:', key, ', Is it a number?', /[0-9]/.test(key))
-
+    console.log('Saved operator', operator)
+    console.log('prevNum number:', prevNum, 'prevNum Type:', typeof(prevNum))
+    console.log('displayValue', displayValue, 'displayValue Type:', typeof(displayValue) )
     const numberPressed = key => {
       if (/\d/.test(key)) {
         return key
@@ -30,35 +36,58 @@ function App() {
 
     switch(key) {
       case numberPressed(key):
-        return setDisplayText(displayText + numberPressed(key))
+        return setDisplayValue(displayValue + numberPressed(key))
       case 'Escape':
-        return setDisplayText('0')
+        setDisplayValue('0')
+        setPrevNum(null)
+        setOperator(null)
+        return
       case '%':
-        return setDisplayText(displayText / 100)
+        setDisplayValue(displayValue / 100)
+        setPrevNum(null)
+        setOperator(null)
+        return 
       case '/':
-        return setDisplayText(key + ' was pressed')
+        setPrevNum(displayValue)
+        setOperator('/')
+        setDisplayValue('0')
+        return 
       case '*':
-        return setDisplayText(key + ' was pressed')
+        setPrevNum(displayValue)
+        setOperator('*')
+        setDisplayValue('0')
+        return 
       case '-':
-        return setDisplayText(key + ' was pressed')
+        setPrevNum(displayValue)
+        setOperator('-')
+        setDisplayValue('0')
+        return 
       case '+':
-        return setDisplayText(key + ' was pressed')
+        setPrevNum(displayValue)
+        setOperator('+')
+        setDisplayValue('0')
+        return 
       case '=':
       case 'Enter':
-        return setDisplayText(key + ' was pressed')
+        if (operator) {
+          return setDisplayValue(evaluate(`${prevNum} ${operator} ${displayValue}`))
+        }
+        setPrevNum(null)
+        setOperator(null)
+        return 
       case '.':
-        if (!displayText.includes('.')) {
-          return setDisplayText(displayText + '.')
+        if (!displayValue.includes('.')) {
+          return setDisplayValue(displayValue + '.')
         }
         return 
       case 'Backspace':
-          if (displayText.length > 1) {
-            return setDisplayText(displayText.slice(0, -1))
-          } else {
-            return setDisplayText('0')
-          }
+        if (displayValue.length > 1) {
+          return setDisplayValue(displayValue.slice(0, -1))
+        } else {
+          return setDisplayValue('0')
+        }
       default:
-        return setDisplayText(displayText)
+        return setDisplayValue(displayValue)
     }
   }
 
@@ -75,28 +104,55 @@ function App() {
 
     switch(value) {
       case 'AC':
-        return setDisplayText('0')
+        setPrevNum(null)
+        setDisplayValue('0')
+        return 
       case '±':
-        return setDisplayText((displayText * -1).toString()) 
+        setPrevNum(null)
+        setDisplayValue((displayValue * -1).toString()) 
+        return 
       case '%':
-        return setDisplayText(displayText / 100)
+        setPrevNum(null)
+        setDisplayValue(displayValue / 100)
+        return 
       case '÷':
-        return 'operator-btn' 
+        setPrevNum(displayValue)
+        setOperator('/')
+        setDisplayValue('0')
+        return
       case '×':
-        return 'operator-btn' 
+        setPrevNum(displayValue)
+        setOperator('*')
+        setDisplayValue('0')
+        return
       case '+':
-        return 'operator-btn'
+        setPrevNum(displayValue)
+        setOperator('+')
+        setDisplayValue('0')
+        return
       case '-':
-        return 'operator-btn'
+        setPrevNum(displayValue)
+        setOperator('-')
+        setDisplayValue('0')
+        return
       case '=':
-        return 'operator-btn'
+        if (operator) {
+          setDisplayValue(evaluate(`${prevNum} ${operator} ${displayValue}`))
+        }
+        setPrevNum(null)
+        setOperator(null)
+        return 
       case '•':
-        if(!displayText.includes('.')) {
-          return setDisplayText(displayText + '.')
+        if(!displayValue.includes('.')) {
+          return setDisplayValue(displayValue + '.')
         }
         return
       default:
-        return setDisplayText(displayText + value)
+        if (displayValue[0] === '0' && !displayValue.includes('.')) {
+          return setDisplayValue(displayValue.slice(1) + value)
+        } else {
+          return setDisplayValue(displayValue + value)
+        }
     }
   }
 
@@ -107,7 +163,7 @@ function App() {
   return (
     <div className="app">
       <div className='display-container'>
-        <Display>{displayText}</Display>
+        <Display>{displayValue}</Display>
       </div>
       <div className='btns-container'>
         {button}
