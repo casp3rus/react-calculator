0fae9a69f841ee45b7eaf622d2393cc7f9d6bc3d
casp3rus <adrian.popan@gmail.com>
2020-07-09 16:51:05 -0500

    remove commafy

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
 
-const keyType = val => {
-    switch(val) {
-        case 'AC':
-            return 'function-btn' 
-        case '±':
-            return 'function-btn' 
-        case '%':
-            return 'function-btn'
-        case '÷':
-            return 'operator-btn' 
-        case '×':
-            return 'operator-btn' 
-        case '+':
-            return 'operator-btn'
-        case '-':
-            return 'operator-btn'
-        case '=':
-            return 'operator-btn'
-        case '0':
-            return 'zero-btn'
-        default:
-            return null
-    }
-}
+const keyType = (val) => {
+  switch (val) {
+    case "AC":
+      return "function-btn";
+    case "±":
+      return "function-btn";
+    case "%":
+      return "function-btn";
+    case "÷":
+      return "operator-btn";
+    case "×":
+      return "operator-btn";
+    case "+":
+      return "operator-btn";
+    case "-":
+      return "operator-btn";
+    case "=":
+      return "operator-btn";
+    case "0":
+      return "zero-btn";
+    default:
+      return null;
+  }
+};
 
 export const Button = ({ children, onButtonClick }) => {
-    return (
-        <div
-            className={`button ${keyType(children)}`}
-            onClick={onButtonClick(children)}
-        >
-            {children}
-        </div>
-    )
-}
+  return (
+    <div
+      className={`button ${keyType(children)}`}
+      onClick={onButtonClick(children)}
+    >
+      {children}
+    </div>
+  );
+};
